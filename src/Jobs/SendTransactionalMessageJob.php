eee47ca206e2b33c1d26443606513eeb70840def
Vu Dang <vudangnt@gmail.com>
2026-05-06 09:00:53 +0700

    feat: add SendTransactionalMessageJob to dispatch messages via queue

---
+++ src/Jobs/SendTransactionalMessageJob.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sendportal\Base\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Sendportal\Base\Models\EmailService;
+use Sendportal\Base\Models\Message;
+use Sendportal\Base\Services\Messages\DispatchMessage;
+
+class SendTransactionalMessageJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $tries = 3;
+
+    protected int $messageId;
+    protected int $emailServiceId;
+
+    public function __construct(int $messageId, int $emailServiceId)
+    {
+        $this->messageId = $messageId;
+        $this->emailServiceId = $emailServiceId;
+    }
+
+    public function handle(DispatchMessage $dispatch): void
+    {
+        /** @var Message|null $message */
+        $message = Message::find($this->messageId);
+
+        /** @var EmailService|null $emailService */
+        $emailService = EmailService::find($this->emailServiceId);
+
+        if (!$message || !$emailService) {
+            return;
+        }
+
+        $dispatch->handle($message, $emailService);
+    }
+}
