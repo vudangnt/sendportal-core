cb11a7770e9e745d7ee233194d02c29c1a068089
Vu Dang <vudangnt@gmail.com>
2026-06-04 18:38:13 +0700

    fix(templates): standalone transactional index reuses shared inheritance grid

--- resources/views/templates/transactional/index.blade.php
+++ resources/views/templates/transactional/index.blade.php
@@ -24,47 +24,6 @@
         </div>
     @endif
 
-    <div class="card">
-        <div class="card-table table-responsive">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Code</th><th>Name</th><th>Source</th>
-                        <th>Last edit</th><th style="width:200px;">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse($templates as $t)
-                    <tr>
-                        <td><code>{{ $t->code }}</code></td>
-                        <td>{{ $t->name }}</td>
-                        <td>
-                            @if($t->source === 'seeded')
-                                <span class="badge badge-light">Seeded</span>
-                            @elseif($t->source === 'modified')
-                                <span class="badge badge-info">&#9998; Modified</span>
-                            @else
-                                <span class="badge badge-warning">&#11088; Custom code</span>
-                            @endif
-                        </td>
-                        <td class="text-muted">{{ $t->updated_at?->diffForHumans() }}</td>
-                        <td>
-                            <a href="{{ route('sendportal.templates.transactional.edit', $t->id) }}"
-                               class="btn btn-sm btn-info"><i class="fas fa-edit"></i></a>
-                            <form action="{{ route('sendportal.templates.transactional.destroy', $t->id) }}"
-                                  method="POST" class="d-inline"
-                                  onsubmit="return confirm('Delete template {{ $t->code }}?');">
-                                @csrf @method('DELETE')
-                                <button class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                    @empty
-                    <tr><td colspan="5" class="text-center text-muted py-4">No transactional templates yet.</td></tr>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
-    </div>
+    @include('sendportal::templates.partials.grid-transactional')
 </div>
 @endsection
